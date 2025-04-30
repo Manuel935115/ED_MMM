e5d8fbf0d0ab81d04e09b3091f8e1fa05b5313b9
Martín Mateos <manuel.martin3@isepceu.es>
2025-04-30 18:46:46 +0200

    MMM Commit de la primera clase persona

---
+++ Persona_MMM/src/recuperacion_entornos/Persona_MMM.java
@@ -0,0 +1,49 @@
+package recuperacion_entornos;
+
+public class Persona_MMM {
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getMasa() {
+		return masa;
+	}
+	public void setMasa(double masa) {
+		this.masa = masa;
+	}
+	public double getAltura() {
+		return altura;
+	}
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	private int id;
+	@Override
+	public String toString() {
+		return "Persona_MMM [id=" + id + ", nombre=" + nombre + ", masa=" + masa + ", altura=" + altura + ", edad="
+				+ edad + ", getId()=" + getId() + ", getNombre()=" + getNombre() + ", getMasa()=" + getMasa()
+				+ ", getAltura()=" + getAltura() + ", getEdad()=" + getEdad() + ", getClass()=" + getClass()
+				+ ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
+	}
+	private String nombre;
+	private double masa;
+	private double altura;
+	private int edad;
+	
+	
+}
